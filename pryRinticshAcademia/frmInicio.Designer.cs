fdec63366e049ea3646716baf6ea33e4d28683b4
Alumno <Alumno@PC68>
2026-05-18 08:22:11 -0300

    Agregar archivos de proyecto.

---
+++ pryRinticshAcademia/frmInicio.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace pryRinticshAcademia
+{
+    partial class frmInicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInicio = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnInicio
+            // 
+            this.btnInicio.Location = new System.Drawing.Point(47, 98);
+            this.btnInicio.Name = "btnInicio";
+            this.btnInicio.Size = new System.Drawing.Size(75, 23);
+            this.btnInicio.TabIndex = 0;
+            this.btnInicio.Text = "Inicio";
+            this.btnInicio.UseVisualStyleBackColor = true;
+            // 
+            // frmInicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 466);
+            this.Controls.Add(this.btnInicio);
+            this.Name = "frmInicio";
+            this.Text = "Bienvenido!!!";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInicio;
+    }
+}
+
